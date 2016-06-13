ae4671f4f299736da9baa327e267c69b5de8d655
itsrainingdata <bryonsgit@gmail.com>
2016-06-13 15:28:01 -0700

    Documentation for pathfinder data

---
+++ R/data.R
@@ -0,0 +1,9 @@
+#' The pathfinder network
+#'
+#' The pathfinder network from the \href{http://www.bnlearn.com/bnrepository/#pathfinder}{Bayesian network repository}.
+#' Pathfinder is an expert system developed in 1992 by Heckerman et. al
+#' to assist with the diagnosis of lymph-node diseases.
+#'
+#' @format An \code{\link{edgeList}} object with 109 nodes and 195 edges.
+#'
+"pathfinder"
