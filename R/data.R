492fa94378f9f39104225971fd5bbf8429bcdf5b
itsrainingdata <bryonsgit@gmail.com>
2016-06-14 16:19:01 -0700

    Add discrete cytometry data

--- R/data.R
+++ R/data.R
@@ -7,3 +7,11 @@
 #' @format An \code{\link{edgeList}} object with 109 nodes and 195 edges.
 #'
 "pathfinder"
+
+#' The cytometry dataset
+#'
+#' A discretized version of the dataset from \href{http://science.sciencemag.org/content/308/5721/523.long}{Sachs et al. (2005)}.
+#'
+#' @format An \code{\link{data.frame}} with 11 variables and 5400 observations.
+#'
+"cytometryDiscrete"
