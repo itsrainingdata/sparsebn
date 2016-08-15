9a08882ab95404780f54c0057e30b210e7e9e537
itsrainingdata <bryonsgit@gmail.com>
2016-08-14 22:32:23 -0400

    Added some basic tests for estimate.dag

--- tests/testthat/test-dag.R
+++ tests/testthat/test-dag.R
@@ -4,5 +4,17 @@ dat_cts <- generate_continuous_sparsebnData()
 dat_disc <- generate_discrete_sparsebnData()
 
 test_that("", {
-    ### Tests added here
+    ### More tests added here
+})
+
+test_that("DAG estimation runs without errors", {
+    expect_error(estimate.dag(dat_cts), NA)  # continuous data
+    expect_error(estimate.dag(dat_disc), NA) # discrete data
+})
+
+test_that("estimate.dag returns expected output", {
+    out <- estimate.dag(dat_cts)
+    expect_is(out, "sparsebnPath")
+    expect_true(check_list_class(as.list(out), "sparsebnFit"))
+    expect_true(is.zero(out[[1]]$edges)) # first estimate should always be null
 })
