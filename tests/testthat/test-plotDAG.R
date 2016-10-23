41e0aceb8440f8c43ca3a1917b5c6052a9afd494
itsrainingdata <bryonsgit@gmail.com>
2016-10-23 12:10:27 -0400

    Removed cyto test

--- tests/testthat/test-plotDAG.R
+++ tests/testthat/test-plotDAG.R
@@ -3,11 +3,9 @@ context("plotDAG")
 el <- generate_fixed_edgeList()
 sbp <- generate_fixed_sparsebnPath()
 sbf <- generate_fixed_sparsebnFit()
-cyto <- sparsebnUtils::edgeList(cyto_edge_list())
 
 test_that("plotDAG runs without errors", {
     expect_error(plotDAG(el), NA)
     expect_error(plotDAG(sbf), NA)
     expect_error(plotDAG(sbp), NA)
-    expect_error(plotDAG(cyto), NA)
 })
