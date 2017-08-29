5e552f912f269d40f3a028d7ce8399adfbfa7273
itsrainingdata <bryonsgit@gmail.com>
2017-08-29 13:29:51 -0400

    Re-factored tests on estimate.dag output

--- tests/testthat/test-dag.R
+++ tests/testthat/test-dag.R
@@ -3,6 +3,19 @@ context("estimate.dag")
 dat_cts <- generate_continuous_sparsebnData()
 dat_disc <- generate_discrete_sparsebnData()
 
+### Helper functions for unit tests
+check_edgelist_integer <- function(out){
+    sapply(out, function(x) all(sapply(x$edges, function(e) is.integer(e))))
+}
+
+check_edgelist_names <- function(out){
+    sapply(out, function(x) !is.null(names(x$edges)))
+}
+
+edgelist_names_equal <- function(out, compare){
+    sapply(out, function(x) isTRUE(all.equal(names(x$edges), compare)))
+}
+
 test_that("", {
     ### More tests added here
 })
@@ -22,24 +35,30 @@ test_that("DAG estimation runs without errors", {
     expect_error(estimate.dag(dat_disc, whitelist = rbind(c(1,2)), blacklist = rbind(c(2,3))), NA) # non-NULL black- AND whitelist
 })
 
-test_that("estimate.dag returns expected output", {
+test_that("estimate.dag returns expected output: Continuous input", {
     ### Continuous data
     out <- estimate.dag(dat_cts)
     expect_is(out, "sparsebnPath")
     expect_true(check_list_class(as.list(out), "sparsebnFit"))
+    expect_true(check_list_class(lapply(out, function(x) x$edges), "edgeList"))
     expect_true(is.zero(out[[1]]$edges)) # first estimate should always be null
 
     # Check that edgeLists have proper format
-    expect_false(all(sapply(out, function(x) is.null(names(x$edges))))) # names is not NULL
-    expect_true(all(sapply(out, function(x) isTRUE(all.equal(names(x$edges), names(dat_cts$data)))))) # names is correct
+    expect_true(all(check_edgelist_integer(out))) # contents are indices, not characters
+    expect_true(all(check_edgelist_names(out))) # names is not NULL
+    expect_true(all(edgelist_names_equal(out, names(dat_cts$data)))) # names is correct
+})
 
+test_that("estimate.dag returns expected output: Discrete input", {
     ### Discrete data
     out <- estimate.dag(dat_disc)
     expect_is(out, "sparsebnPath")
     expect_true(check_list_class(as.list(out), "sparsebnFit"))
+    expect_true(check_list_class(lapply(out, function(x) x$edges), "edgeList"))
     expect_true(is.zero(out[[1]]$edges)) # first estimate should always be null
 
     # Check that edgeLists have proper format
-    expect_false(all(sapply(out, function(x) is.null(names(x$edges))))) # names is not NULL
-    expect_true(all(sapply(out, function(x)  isTRUE(all.equal(names(x$edges), names(dat_cts$data)))))) # names is correct
+    expect_true(all(check_edgelist_integer(out))) # contents are indices, not characters
+    expect_true(all(check_edgelist_names(out))) # names is not NULL
+    expect_true(all(edgelist_names_equal(out, names(dat_disc$data)))) # names is correct
 })
